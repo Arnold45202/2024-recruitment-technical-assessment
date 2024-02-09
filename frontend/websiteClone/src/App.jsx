e333ec61fa2c4e9c8cf9532c09de25392e89a84e
Jackson <kahowang3659@gmail.com>
2024-02-09 23:20:20 +1100

    adding the core code of the environement and adding the main logo of the project and the nav bar

---
+++ frontend/websiteClone/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import mainLogo from './assets/unilectives.svg';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='left-bar'>
+        <div className='logo-div'>
+          <div className='logo-div2'>
+            <a>
+              <img src={mainLogo} alt="logo" className='main-logo'/>
+            </a>
+          </div>
+        </div>
+        <div className='top-part'>
+          <div className='top-part2'>
+            <div className='book-part'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
+
